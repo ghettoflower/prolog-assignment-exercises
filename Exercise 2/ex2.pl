a41b62247323aaa300de1c95cec7d3370226e509
Jake Sant <jake.sant.18@um.edu.mt>
2019-11-05 10:41:35 +0100

    Answers 2, 3 and 5 updated, Answer 4 halfway done

--- Exercise 2/ex2.pl
+++ Exercise 2/ex2.pl
@@ -1,14 +1,18 @@
 qr(N1,N2,Quotient,Remainder):- (Quotient is N1//N2, Remainder is N1 mod N2).
-qr(N1,N2):- (Quotient is N1//N2, Remainder is N1 mod N2), write('The quotient is '), write(Quotient), nl, write('The remainder is '), write(Remainder), nl.
+
+qr(N1,N2):- qr(N1,N2,Quotient,Remainder), write(N1), write(' divided by '), write(N2), write(' is '),
+write(Quotient), write(' Remainder '), write(Remainder), nl.
+
+/*qr(N1,N2):- (Quotient is N1//N2, Remainder is N1 mod N2), write('The quotient is '), write(Quotient), nl, write('The remainder is '), write(Remainder), nl.
+*/
 
 q3():- write('Enter the first number'), read(N1), nl, write('Enter the second number'), read(N2), nl,
-write('The quotient is '), quotient(X), write(X), nl,
-write('The remainder is '), remainder(Y), write(Y), nl.
+write('The quotient is '), Q is N1//N2, write(Q), nl,
+write('The remainder is '), R is N1 mod N2, write(R), nl.
 
-quotient(X):- X is N1//N2.
-remainder(Y):- Y is N1 mod N2.
-
-q5():- write('Enter the lenth of the rectangle'), read(LENGTH), nl,
-write('Enter the width of the rectange'), read(WIDTH), nl,
-area(LENGTH, WIDTH, Z), write('The area of the rectangle is '), write(Z), write(' square units'), nl.
+q5():- write('Input length'), read(LENGTH),
+write('Input width'), read(WIDTH),
+area(LENGTH, WIDTH, Z), write('Area of rectangle is '), write(Z), nl,
+perimeter(LENGTH, WIDTH, P), write('Perimeter is '), write(P), nl.
 area(X, Y, Z):- Z is X*Y.
+perimeter(X, Y, Z):- Z is (X+Y) * 2.
