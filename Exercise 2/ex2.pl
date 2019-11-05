e5e2900ac6623b156cdf9733d3a4a1bc3c792441
Jake Sant <jake.sant.18@um.edu.mt>
2019-11-05 10:54:06 +0100

    Finished Answer 4 and ergo all Answers

--- Exercise 2/ex2.pl
+++ Exercise 2/ex2.pl
@@ -6,9 +6,23 @@ write(Quotient), write(' Remainder '), write(Remainder), nl.
 /*qr(N1,N2):- (Quotient is N1//N2, Remainder is N1 mod N2), write('The quotient is '), write(Quotient), nl, write('The remainder is '), write(Remainder), nl.
 */
 
-q3():- write('Enter the first number'), read(N1), nl, write('Enter the second number'), read(N2), nl,
+q3():- write('Enter the dividend'), read(N1), nl, write('Enter the divisor'), read(N2), nl,
 write('The quotient is '), Q is N1//N2, write(Q), nl,
 write('The remainder is '), R is N1 mod N2, write(R), nl.
+
+q4():- write('Enter a number'), read(N1), nl, write('Another number'), read(N2), nl,
+write('Which operation do you wish to perform?'), nl,
+write('a. Add'), nl, write('s. Subtract'), nl,
+write('m. Multiply'), nl, write('d. Divide'), nl,
+write('e. Exit'), nl,
+write('Enter either a, s, m, d or e'), read(X), nl,
+option(X, N1, N2), nl.
+
+option(a, N1, N2):- N3 is N1+N2, write(N1), write(' + '), write(N2), write(' = '), write(N3).
+option(s, N1, N2):- N3 is N1-N2, write(N1), write(' - '), write(N2), write(' = '), write(N3).
+option(m, N1, N2):- N3 is N1*N2, write(N1), write(' * '), write(N2), write(' = '), write(N3).
+option(d, N1, N2):- N3 is N1//N2, N4 is N1 mod N2, write(N1), write(' ÷ '), write(N2), write(' = '), write(N3), write(' remainder '), write(N4).
+option(e, N1, N2):- write('Exiting...').
 
 q5():- write('Input length'), read(LENGTH),
 write('Input width'), read(WIDTH),
